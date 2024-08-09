e3a6fa3917428d6ae4666e70341efd0b97791553
Stasoon <stas.csa@ya.ru>
2024-08-09 22:09:39 +0300

    set language from tg

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -1,30 +1,30 @@
-// Функция для получения языка из URL хеша
-function getLanguageFromHash() {
-    const hash = window.location.hash;
-    if (hash) {
-        return hash.substring(1); // Удаляем символ '#'
-    }
-    return "ru";
-}
+// // Функция для получения языка из URL хеша
+// function getLanguageFromHash() {
+//     const hash = window.location.hash;
+//     if (hash) {
+//         return hash.substring(1); // Удаляем символ '#'
+//     }
+//     return "ru";
+// }
 
-// Получаем язык из URL
-const language = getLanguageFromHash();
+// // Получаем язык из URL
+// const language = getLanguageFromHash();
 
-// Функция для обновления всех ссылок на странице с текущим хешем
-function updateLinksWithLanguageHash() {
-    const links = document.querySelectorAll('a[href]');
+// // Функция для обновления всех ссылок на странице с текущим хешем
+// function updateLinksWithLanguageHash() {
+//     const links = document.querySelectorAll('a[href]');
     
-    links.forEach(link => {
-        link.href = `${link.href}#${language}`;
-    });
-}
+//     links.forEach(link => {
+//         link.href = `${link.href}#${language}`;
+//     });
+// }
 
-// Обновляем ссылки при загрузке страницы
-updateLinksWithLanguageHash();
+// // Обновляем ссылки при загрузке страницы
+// updateLinksWithLanguageHash();
 
 
 // Полное открытие окна WebApp
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-alert(tg.initDataUnsafe.user.language_code);
+const language = tg.initDataUnsafe.user.language_code;
